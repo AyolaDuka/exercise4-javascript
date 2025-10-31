9e248c79b41570bb6391a980f7032e97b2ac1c05
AyolaDuka <ayoladuka0@gmail.com>
2025-10-31 12:30:37 +0200

    I used conditionals on JavaScript

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+let score  = 78
+let passingGrade = 60
+if(score == 100)
+    console.log("You did great , you got a perferct score!")
+else if(score >= passingGrade)
+    console.log("You have met the passing grade , well done!")
+else
+    console.log("Unfortuantely you have failed, better luck next time!")
